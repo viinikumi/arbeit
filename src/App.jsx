272f98fe801146f8fd8ed3b2eccf1f4111e44a3e
viinikumi <miikka.vainionp@outlook.com>
2025-11-04 23:22:16 +0200

    firebasesaantomuutos App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,75 +3,78 @@ import {
   Routes,
   Route,
   NavLink,
-  useLocation,
 } from "react-router-dom";
-/*import { useEffect, useState } from "react";*/
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./firebaseConfig";
+
 import Muuta from "./sivut/Muuta";
 import Vaunut from "./sivut/Vaunut";
 import Vuorohaku from "./sivut/Vuorohaku";
 import Alku from "./sivut/Alku";
 import "./tyyli.css";
 import "./muuttuvat.css";
-/*import Signup from "./components/Signup"; */
 import Login from "./components/Login";
 import UserInfo from "./components/UserInfo";
 import KellonAika from "./components/KellonAika";
 
-const AuthComponents = () => {
-  const location = useLocation();
-  return location.pathname === "/" ? (
-    <>
-      <h6 className="logOtsikko">Kirjautuminen</h6>
-      <div>
-        <Login />
-        {/*<Signup />*/}
-      </div>
-    </>
-  ) : null;
-};
-
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Kuunnellaan Firebase Auth -tilaa
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
   const asetaVari = (vari, teksti) => {
-    const uusiVari = vari; // Uusi väri
-    const tekstiVari = teksti;
-    document.documentElement.style.setProperty("--primary", uusiVari);
-    document.documentElement.style.setProperty("--teksti", tekstiVari);
+    document.documentElement.style.setProperty("--primary", vari);
+    document.documentElement.style.setProperty("--teksti", teksti);
   };
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
+  if (loading) {
+    return <p>Ladataan...</p>;
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h6 className="logOtsikko">Kirjautuminen</h6>
+        <Login />
+      </div>
+    );
+  }
+
+  // Jos käyttäjä on kirjautunut:
   return (
     <>
       <BrowserRouter basename="/arbeit">
-        <div style={{ position: "static", zIndex: "7", maxHeight: "700px" }}>
-          <AuthComponents />
-        </div>
-        <div className="boxi"></div>
         <div className="ylapalk"></div>
-
         <div className="alapalk"></div>
 
         <nav style={{ marginLeft: "5px" }}>
           <UserInfo />
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
+          <button onClick={handleLogout} className="logBut" style={{ marginLeft: "10px" }}>
+            Kirjaudu ulos
+          </button>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
             Alku&nbsp;
           </NavLink>
-          <NavLink
-            to="/Vuorohaku"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
+          <NavLink to="/Vuorohaku" className={({ isActive }) => (isActive ? "active" : "")}>
             | Vuoro |
           </NavLink>
-          <NavLink
-            to="/Vaunut"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
+          <NavLink to="/Vaunut" className={({ isActive }) => (isActive ? "active" : "")}>
             &nbsp;Vaunut&nbsp;
           </NavLink>
-          <NavLink
-            to="/Muuta"
-            className={({ isActive }) => (isActive ? "active" : "")}
-          >
+          <NavLink to="/Muuta" className={({ isActive }) => (isActive ? "active" : "")}>
             | Muuta
           </NavLink>
         </nav>
@@ -84,33 +87,14 @@ function App() {
           <Route path="*" element={<Alku />} />
         </Routes>
       </BrowserRouter>
+
       <div className="aladiv">
-        <KellonAika style={{ zIndex: "7" }} />
+        <KellonAika />
         <div className="varit">
-          <div
-            className="vari0"
-            onClick={() => asetaVari("rgb(115, 147, 179)", "black")}
-          ></div>
-          <div
-            className="vari1"
-            onClick={() => asetaVari("rgb(0, 71, 171)", "white")}
-          ></div>
-          {/*<div
-            className="vari2"
-            onClick={() => asetaVari("rgb(0, 255, 255)")}
-  ></div> */}
-          <div
-            className="vari3"
-            onClick={() => asetaVari("rgb(53, 57, 53)", "white")}
-          ></div>
-          <div
-            className="vari4"
-            onClick={() => asetaVari("rgb(37, 199, 75)", "black")}
-          ></div>
-          {/*<div
-            className="vari5"
-            onClick={() => asetaVari("rgb(204, 204, 255)")}
-></div> */}
+          <div className="vari0" onClick={() => asetaVari("rgb(115, 147, 179)", "black")}></div>
+          <div className="vari1" onClick={() => asetaVari("rgb(0, 71, 171)", "white")}></div>
+          <div className="vari3" onClick={() => asetaVari("rgb(53, 57, 53)", "white")}></div>
+          <div className="vari4" onClick={() => asetaVari("rgb(37, 199, 75)", "black")}></div>
         </div>
       </div>
     </>
